94d5d2483d28980352ee14208f53709ac668477e
Yury <yelmuf@gmail.com>
2016-08-14 15:53:14 +0200

    Added admin articles management page (without func)

---
+++ views/articles_admin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru, en">
+  <head>
+    <meta charset="utf-8">
+    <title>Articles - Admin view</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+  </head>
+
+  <body>
+
+    <div class="container">
+        <h1>Мой первый бл*г</h1>
+    </div>
+    <div class="breadcrumb container-fluid">
+      <a class="btn btn-primary" href="index.php?action=add">Добавить статью</a>
+    </div>
+    <div class="container-fluid">
+      <table class="admin-table table-responsive table-condensed table-bordered table-hover">
+          <tr>
+            <th>Дата</th>
+            <th>ID</th>
+            <th>Заголовок</th>
+            <th></th>
+            <th></th>
+          </tr>
+        <?php foreach ($articles as $a): ?>
+          <tr>
+            <td><?=$a['date'] ?></td>
+            <td><?=$a['id'] ?></td>
+            <td><?=$a['title'] ?></td>
+            <td><a class="btn" href="index.php?action=edit?id=<?=$a['id']?>">Редактировать</a></td>
+            <td><a class="btn" href="index.php?action=delete?id=<?=$a['id']?>">Удалить</a></td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    </div>
+
+<footer class="footer">
+  <p class="text-center">Слил over 20 php файлов в кучу
+    <br /> - сделал страничку
+    <br /> Copyleft © 2016</p>
+</footer>
+</body>
+
+</html>
